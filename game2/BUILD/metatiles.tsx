0515cbd79c44ce1ac4e9c01a21b4be804d603ad7
jgonz131 <57538216+jgonz131@users.noreply.github.com>
2021-12-08 19:12:08 -0600

    Delete game2 directory

--- game2/BUILD/metatiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="metatiles" tilewidth="32" tileheight="32" tilecount="128" columns="16">
- <image source="../../../../../Downloads/metatiles.png" width="512" height="256"/>
-</tileset>
